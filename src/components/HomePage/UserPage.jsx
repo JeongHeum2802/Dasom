341e2ef9321afa31639f7784395e48640e44a37d
최정흠 <wjdgma60@gmail.com>
2025-08-04 12:12:48 +0900

    proxy수정_및_back데이터_확인

--- src/components/HomePage/UserPage.jsx
+++ src/components/HomePage/UserPage.jsx
@@ -6,12 +6,12 @@ export default function UserPage({ userData, onCloseUserPage }) {
       <button onClick={()=>onCloseUserPage(-1)} className="absolute top-6 right-6 text-gray-500 hover:text-gray-800 text-4xl font-bold">&times;</button>
       <div className="flex-1 flex flex-col items-center justify-center p-12">
         <img
-          src={userData.imgsrc}
+          src={userData.main.profileImageUrl}
           alt={`Profile of ${userData.name}`}
           className="w-90 h-90 rounded-full border-8 border-pink-200 shadow-lg mb-12"
         />
-        <h1 className="text-5xl font-bold mb-4 text-pink-500">{userData.name}</h1>
-        <p className="text-2xl text-center max-w-2xl mt-6 text-gray-700">{userData.message}</p>
+        <h1 className="text-5xl font-bold mb-4 text-pink-500">{userData.main.name}</h1>
+        <p className="text-2xl text-center max-w-2xl mt-6 text-gray-700">{userData.main.message}</p>
       </div>
       <div className="bg-pink-200 p-6 flex justify-around">
         <button className="bg-pink-400 hover:bg-pink-500 text-white font-bold py-3 px-8 rounded-full transition duration-300">
